1d477789ca0eb29297bbb9691bbd9a0d4addf6a1
Rashid Mohammed Iliyas Khan <khanr7433@gmail.com>
2025-07-22 16:25:00 +0530

    Implement email verification API with token validation and error handling

---
+++ src/app/api/users/verigyEmail/route.ts
@@ -0,0 +1,44 @@
+import { connectDB } from "@/db/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/model/userModel";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+
+    const { token } = reqBody;
+
+    if (!token) {
+      return NextResponse.json({ error: "Token is required" }, { status: 400 });
+    }
+
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Invalid token" }, { status: 400 });
+    }
+
+    user.isVerfied = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+    await user.save();
+
+    return NextResponse.json({
+      message: "Email verified successfully",
+      success: true,
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json(
+      { error: "Error while verifying email" },
+      { status: 500 }
+    );
+  }
+}
