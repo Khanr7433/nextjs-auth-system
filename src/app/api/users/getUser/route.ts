d3a79c3ddc5bf62f638f879ee2ab4b0f514cd281
Rashid Mohammed Iliyas Khan <khanr7433@gmail.com>
2025-07-22 16:28:37 +0530

    Implement GET user data API with authentication and error handling

---
+++ src/app/api/users/getUser/route.ts
@@ -0,0 +1,37 @@
+import { getDataFromToken } from "@/helpers/grtDataFromToken";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/model/userModel";
+import { connectDB } from "@/db/dbConfig";
+
+connectDB();
+
+export async function GET(request: NextRequest) {
+  try {
+    const userId = await getDataFromToken(request);
+
+    if (!userId) {
+      return NextResponse.json(
+        { error: "User not authenticated" },
+        { status: 401 }
+      );
+    }
+    const user = await User.findOne({ _id: userId }).select("-password");
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({
+      message: "User's data retrieved successfully",
+      data: user,
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 400 });
+    }
+    return NextResponse.json(
+      { error: "Error while retrieving user data" },
+      { status: 500 }
+    );
+  }
+}
