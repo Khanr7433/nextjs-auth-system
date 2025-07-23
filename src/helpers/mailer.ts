20020b69f0f7fbabae8e69593c5078ebfda99972
Rashid Mohammed Iliyas Khan <khanr7433@gmail.com>
2025-07-23 09:40:13 +0530

    Update email links to use DOMAIN environment variable instead of BACKEND_URL

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -41,8 +41,8 @@ export const sendEmail = async ({
       to: email,
       subject:
         emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-      html: `<p>Click <a href="${process.env.BACKEND_URL}/${emailType === "VERIFY" ? "verifyEmail" : "resetPassword"}?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
-            or copy and paste the link below in your browser. <br> ${process.env.BACKEND_URL}/${emailType === "VERIFY" ? "verifyEmail" : "resetPassword"}?token=${hashedToken}
+      html: `<p>Click <a href="${process.env.DOMAIN}/${emailType === "VERIFY" ? "verifyEmail" : "resetPassword"}?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/${emailType === "VERIFY" ? "verifyEmail" : "resetPassword"}?token=${hashedToken}
             </p>`,
     };
 
